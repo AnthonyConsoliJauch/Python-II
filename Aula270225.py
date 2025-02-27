8efb79bcdbae9d876388154884fdbad33e618eaf
Anthony Consoli <anthony.cjauch@senacsp.edu.br>
2025-02-27 15:58:56 -0300

    enviando pastas de projeto

---
+++ Aula270225.py
@@ -0,0 +1,6 @@
+nota1 = int(input("Professor, insira a primeira nota do aluno(a)"))
+nota2 = int(input("Professor, insira a segunda nota do aluno(a)"))
+nome = (input("Professor, insira o nome do aluno(a)"))
+media = (nota1 + nota2) / 2
+print(media)
+print("Olá aluno", nome, "sua Média final é de ", media)
