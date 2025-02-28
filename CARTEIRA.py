8d849479fa1647736140682f0a6df23e9e0b9c6a
Anthony Consoli <anthony.cjauch@senacsp.edu.br>
2025-02-28 14:33:04 -0300

    Atualizações

---
+++ CARTEIRA.py
@@ -0,0 +1,7 @@
+nome=(input)("Qual seu nome?")
+idade=int(input("Qual sua idade?"))
+tem_carteira=input("Tem carteira de motorista? Digite s para SIM, e n para NÃO.")
+if idade >=18 and tem_carteira == 's':
+    print("Pode dirigir.")
+else:
+    print("Não pode dirigir.")
